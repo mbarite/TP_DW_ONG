49d80df63d7804534ec096aea07d07805a57deb3
Rodrigo <83443307+RodrigoRajski@users.noreply.github.com>
2024-06-10 18:48:47 -0300

    se invierten colores modo oscuro

--- dark.js
+++ dark.js
@@ -1,6 +1,9 @@
 let darkmode_toggle=document.getElementById('toggle');
-let label
+let header=document.querySelector('header')
+let footer=document.querySelector('footer')
 toggle.addEventListener('change',(event)=>{
     let cheked=event.target.cheked;
     document.body.classList.toggle('dark');
+    header.classList.toggle('dark');
+    footer.classList.toggle('dark');
 })
